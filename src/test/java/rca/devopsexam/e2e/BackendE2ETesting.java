f709d397dce345cb2a39976425c0ba7e85fa446c
Iriza-peace <irizapeace.com>
2023-12-18 10:53:33 +0200

    Added and Tested other scenarios for end to end testing

--- src/test/java/rca/devopsexam/e2e/BackendE2ETesting.java
+++ src/test/java/rca/devopsexam/e2e/BackendE2ETesting.java
@@ -1,4 +1,5 @@
 package rca.devopsexam.e2e;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,12 +11,14 @@ import rca.devopsexam.v1.payload.ApiResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BackendE2ETesting {
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void testDoMathOperation_E2E() {
+    public void testDoMathOperation_Addition_E2E() {
         // Given
         DoMathRequestDto dto = new DoMathRequestDto(2, 5, "+");
 
@@ -26,6 +29,51 @@ public class BackendE2ETesting {
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(true, response.getBody().getSuccess());
         assertEquals("Success", response.getBody().getMessage());
-        assertEquals(7.0, response.getBody().getData()); // Adjust based on your actual response structure
+        assertEquals(7.0, response.getBody().getData());
     }
+
+    @Test
+    public void testDoMathOperation_Subtraction_E2E() {
+        // Given
+        DoMathRequestDto dto = new DoMathRequestDto(10, 5, "-");
+
+        // When
+        ResponseEntity<ApiResponse> response = restTemplate.postForEntity("/api/v1/do_math", dto, ApiResponse.class);
+
+        // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(true, response.getBody().getSuccess());
+        assertEquals("Success", response.getBody().getMessage());
+        assertEquals(5.0, response.getBody().getData());
     }
+
+    @Test
+    public void testDoMathOperation_Multiplication_E2E() {
+        // Given
+        DoMathRequestDto dto = new DoMathRequestDto(3, 4, "*");
+
+        // When
+        ResponseEntity<ApiResponse> response = restTemplate.postForEntity("/api/v1/do_math", dto, ApiResponse.class);
+
+        // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(true, response.getBody().getSuccess());
+        assertEquals("Success", response.getBody().getMessage());
+        assertEquals(12.0, response.getBody().getData());
+    }
+
+    @Test
+    public void testDoMathOperation_Division_E2E() {
+        // Given
+        DoMathRequestDto dto = new DoMathRequestDto(15, 3, "/");
+
+        // When
+        ResponseEntity<ApiResponse> response = restTemplate.postForEntity("/api/v1/do_math", dto, ApiResponse.class);
+
+        // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(true, response.getBody().getSuccess());
+        assertEquals("Success", response.getBody().getMessage());
+        assertEquals(5.0, response.getBody().getData());
+    }
+}
